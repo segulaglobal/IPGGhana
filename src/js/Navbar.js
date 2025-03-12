14800513e9cae3c3c643f2bbaa4389f0234cf73b
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2025-03-12 10:16:16 +0000

    navigation for mobile completed

--- src/js/Navbar.js
+++ src/js/Navbar.js
@@ -3,13 +3,15 @@ const navButton = document.querySelector(".nav-btn");
 const navList = document.querySelector(".nav-list");
 const navOverlay = document.querySelector(".nav-overlay");
 const navItem = document.querySelectorAll(".nav-item");
-const body = document.body;
 
 
 let showMenu = false;
 navButton.addEventListener("click", openMenu);
-// navButton.addEventListener("click", console.log("you clicked"));
-navOverlay.addEventListener("click", openMenu);
+// navList.addEventListener("click", openMenu);
+
+function click() {
+    alert("you clicked");
+}
 
 
 function openMenu () {
@@ -20,8 +22,8 @@ function openMenu () {
         navItem.forEach(item => {
             item.classList.add("show");
         });
-        
-        body.classList.add("no-scroll");
+        document.body.classList.add("remove-scrolling"); 
+        // document.body.classList.add("no-scroll");
         showMenu = true;
     }else {
         navButton.classList.remove("close");
@@ -31,7 +33,31 @@ function openMenu () {
         navItem.forEach(item => {
             item.classList.remove("show");
         });
-        body.classList.remove("no-scroll");
+        document.body.classList.remove("remove-scrolling"); 
+        // document.body.classList.remove("no-scroll");
         showMenu = false;
     }
 }
+
+
+
+
+
+
+// ============== DropDown functionality =============
+document.addEventListener("click", e => {
+    const isDropdownButton = e.target.matches("[data-dropdown-button]")
+    if(!isDropdownButton && e.target.closest("[data-dropdown]") != null)
+    return
+
+    let currentDropdown
+    if(isDropdownButton) {
+        currentDropdown = e.target.closest('[data-dropdown]')
+        currentDropdown.classList.toggle('active')
+    }
+
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+        if (dropdown === currentDropdown) return
+        dropdown.classList.remove("active")
+    })
+})
